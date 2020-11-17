a636d6bd93bade79b43cd37eaf4c2434a04098e6
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-17 20:57:34 +0100

    Home - index.jsx and styles

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { render } from 'react-dom';
-import './index.html';
+import './style.css';
+import title_img from '../img/title.png';
 
-render(
-  <>
-    <header>
-      <h1>React Starter</h1>
-    </header>
-    <main>
-      <p>Moje první React stránka</p>
-    </main>
-    <footer>Martin Podloucký</footer>
-  </>,
-  document.querySelector('#app'),
-);
+const Home = () => {
+  return (
+    <div className="home">
+      <img className="title__img" src={title_img} alt="titulní obrázek" />
+      <div className="title">
+        <h1 className="title__heading">KÓDOVÁNÍ PRO DĚTI</h1>
+        <button className="btn">pojď to zkusit</button>
+      </div>
+    </div>
+  );
+};
+export default Home;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Home from './Home/index.jsx';
+import './style.css';
 import './index.html';
 
-render(
-  <>
-    <header>
-      <h1>React Starter</h1>
-    </header>
-    <main>
-      <p>Moje první React stránka</p>
-    </main>
-    <footer>Martin Podloucký</footer>
-  </>,
-  document.querySelector('#app'),
-);
+const App = () => {
+  return <Home />;
+};
+render(<App />, document.querySelector('#app'));
