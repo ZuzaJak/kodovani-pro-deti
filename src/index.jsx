57f46fb85e99bfe3328cb963615882ed34373b0f
Lena Bacikova <lena.bacikova1@gmail.com>
2020-11-18 11:37:34 +0100

    wiki stranka

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from './Home/index.jsx';
+import Wiki from './Wiki/index.jsx';
+
 import './style.css';
 import './index.html';
 
-const App = () => {
-  return <Home />;
-};
+const App = () => (
+  <>
+    <Home />
+    <Wiki />
+  </>
+)
 render(<App />, document.querySelector('#app'));
