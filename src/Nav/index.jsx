8705a06fbd6133e49daf78a73fab5b284a527261
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-18 16:09:45 +0100

    position sticky works

--- src/Nav/index.jsx
+++ src/Nav/index.jsx
@@ -3,23 +3,21 @@ import './style.css';
 
 const Nav = () => {
   return (
-    <div className="navbar__container">
-      <div className="navbar">
-        <div className="navbar__left">{'<kódování pro děti/>'}</div>
-        <div className="navbar__right">
-          <a className="navbar__right-item" href="#">
-            o projektu
-          </a>
-          <a className="navbar__right-item" href="#">
-            kódovací wikipedie
-          </a>
-          <a className="navbar__right-item" href="#">
-            vyzkoušej to
-          </a>
-          <a className="navbar__right-item" href="#">
-            kam dál
-          </a>
-        </div>
+    <div className="navbar">
+      <div className="navbar__left">{'<kódování pro děti/>'}</div>
+      <div className="navbar__right">
+        <a className="navbar__right-item" href="#">
+          o projektu
+        </a>
+        <a className="navbar__right-item" href="#">
+          kódovací wikipedie
+        </a>
+        <a className="navbar__right-item" href="#">
+          vyzkoušej to
+        </a>
+        <a className="navbar__right-item" href="#">
+          kam dál
+        </a>
       </div>
     </div>
   );
