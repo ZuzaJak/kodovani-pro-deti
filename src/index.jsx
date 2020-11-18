8393c71ee8bd95869595e09a46694b8dcce521f9
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-18 11:46:46 +0100

    Header

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-<<<<<<< HEAD
-import Home from './Home/index.jsx';
+import Header from './Header/index.jsx';
 import Wiki from './Wiki/index.jsx';
 
 import './style.css';
@@ -9,17 +8,8 @@ import './index.html';
 
 const App = () => (
   <>
-    <Home />
+    <Header />
     <Wiki />
   </>
-)
-=======
-import Header from './Header/index.jsx';
-import './style.css';
-import './index.html';
-
-const App = () => {
-  return <Header />;
-};
->>>>>>> c56c22a69c4455904f6d1818aa0fd495a939ddd6
+);
 render(<App />, document.querySelector('#app'));
