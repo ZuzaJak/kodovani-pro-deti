bc82878753f17f532dca736dae6bc138224242b1
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-18 14:02:45 +0100

    Nav, position sticky not working yet

--- src/Nav/index.jsx
+++ src/Nav/index.jsx
@@ -1,2 +1,27 @@
 import React from 'react';
 import './style.css';
+
+const Nav = () => {
+  return (
+    <div className="navbar__container">
+      <div className="navbar">
+        <div className="navbar__left">{'<kódování pro děti/>'}</div>
+        <div className="navbar__right">
+          <a className="navbar__right-item" href="#">
+            o projektu
+          </a>
+          <a className="navbar__right-item" href="#">
+            kódovací wikipedie
+          </a>
+          <a className="navbar__right-item" href="#">
+            vyzkoušej to
+          </a>
+          <a className="navbar__right-item" href="#">
+            kam dál
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Nav;
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import Header from './Header/index.jsx';
 import Wiki from './Wiki/index.jsx';
+import Nav from './Nav/index.jsx';
 
 import './style.css';
 import './index.html';
 
 const App = () => (
   <>
+    <Nav />
     <Header />
     <Wiki />
   </>
