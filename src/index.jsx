ff80abb344ec83f4a5533d7d4cf19011ab8b61d6
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-18 19:22:03 +0100

    changes in styles - Header, Nav, main index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import Header from './Header/index.jsx';
 import Wiki from './Wiki/index.jsx';
 import Nav from './Nav/index.jsx';
+import About from './About/index.jsx';
+import Links from './Links/index.jsx';
 
 import './style.css';
 import './index.html';
@@ -11,7 +13,9 @@ const App = () => (
   <>
     <Nav />
     <Header />
+    <About />
     <Wiki />
+    <Links />
   </>
 );
 render(<App />, document.querySelector('#app'));
