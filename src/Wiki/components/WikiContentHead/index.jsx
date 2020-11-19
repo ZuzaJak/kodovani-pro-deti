01b3c31eb5b24cd4d3f8b785ba616282f47dbc60
Lena Bacikova <lena.bacikova1@gmail.com>
2020-11-19 10:16:01 +0100

    struktura pro WIKI stranku

---
+++ src/Wiki/components/WikiContentHead/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.css';
+
+
+const WikiContentHead = () => {
+  return (
+    <div className="wiki">
+      <div className="wiki__content">
+        <div className="wiki__title-flex">
+          <span className="bracket"> {'{'}</span>
+          <div className="wiki__title-bracket-flex">
+            <h1 className="wiki__title">LOREM IPSUM</h1>
+            <p className="wiki__title-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+      );
+};
+
+export default WikiContentHead;
--- src/Wiki/index.jsx
+++ src/Wiki/index.jsx
@@ -1,66 +1,18 @@
 import React from 'react';
+import WikiContentHead from './components/WikiContentHead';
+import WikiContentSection from './components/WikiContentSection';
 import './style.css';
 
 const Wiki = () => {
   return (
-    <div className="wiki">
-      <div className="wiki__content">
-        <div className="wiki__title-flex">
-          <span className="bracket"> {'{'}</span>
-          <div className="wiki__title-bracket-flex">
-            <h1 className="wiki__title">LOREM IPSUM</h1>
-            <p className="wiki__title-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="wiki__sections">
-        <div className="wiki__section">
-          <h2 className="wiki__section-header"> header </h2>
-          <p className="wiki__section-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
-          </p>
-        </div>
-
-        <div className="wiki__section">
-          <h2 className="wiki__section-header"> header </h2>
-          <p className="wiki__section-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
-          </p>
-        </div>
-        <div className="wiki__section">
-          <h2 className="wiki__section-header"> header </h2>
-          <p className="wiki__section-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
-          </p>
-        </div>
-      </div>
-    </div>
+    <>
+    <main>
+      <WikiContentHead />
+      <WikiContentSection
+        snippet="Code_snippet_01"
+      />
+    </main>
+    </>
   );
 };
 
