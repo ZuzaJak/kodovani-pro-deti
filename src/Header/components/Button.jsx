548a23e3a1f66497c434b6707dd94cf6b9bc685e
Lena Bacikova <lena.bacikova1@gmail.com>
2020-11-26 10:45:48 +0100

    zmena stylu button material

--- src/Header/components/Button.jsx
+++ src/Header/components/Button.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({}))
+const useStyles = makeStyles((theme) => ({
+root: {
+  color: "white",
+  background: "#0d0638",
+  height: 68,
+  padding: "0 50px",
+  border: "none",
+  borderRadius: 3,
+}
+
+}))
 
 
 export default function ContainedButtons() {
   const classes = useStyles();
 
   return (
-      <Button variant="contained" color="primary" href="#contained-buttons">
-        Pojd si zahrat
+    <>
+      <Button
+      classes={{
+        root: classes.root
+      }}
+      variant="contained"
+      color="primary"
+      href="#contained-buttons"
+      >
+        Pojď si zahrát
       </Button>
+
+    </>
   );
 }
