a7d9435bc9239e373316bd4bfe134f74200f3db1
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-27 20:47:53 +0100

    oprava cesty

--- src/Wiki/wiki.jsx
+++ src/Wiki/wiki.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import WikiContentHead from './components/WikiContentHead/wiki_head.jsx';
 import WikiContentSection from './components/WikiContentSection/wiki_section.jsx';
 // import snippet from './components/WikiContentSection/index.jsx';
-import WikiContentNavigation from './components/WikiContentNAvigation/wiki_navigation.jsx';
+import WikiContentNavigation from './components/WikiContentNavigation/wiki_navigation.jsx';
 
 const Wiki = () => {
   return (
