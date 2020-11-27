5387a9bfaba46f41c4beca9120d2e582b22893c5
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-27 15:34:15 +0100

    wiki navigation

--- src/Wiki/wiki.jsx
+++ src/Wiki/wiki.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 import WikiContentHead from './components/WikiContentHead/wiki_head.jsx';
 import WikiContentSection from './components/WikiContentSection/wiki_section.jsx';
 // import snippet from './components/WikiContentSection/index.jsx';
-import WikiContentNavigation from './components/WikiContentNAvigation/wiki_navigation.jsx';
+/* import WikiContentNavigation from './components/WikiContentNAvigation/wiki_navigation.jsx'; */
 
 const Wiki = () => {
   return (
     <>
-    <div id="wiki">
-      <main>
-        <WikiContentHead />
-        <WikiContentNavigation />
-        <Button />
-        <WikiContentSection /* snippet={snippet} */ />
-      </main>
-    </div>
+      <div id="wiki">
+        <main>
+          <WikiContentHead />
+          {/*   <WikiContentNavigation /> */}
+          <Button />
+          <WikiContentSection /* snippet={snippet} */ />
+        </main>
+      </div>
     </>
   );
 };
