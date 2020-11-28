3ffa84824013afe7cd273279b7c0dcd2ad42ffb3
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-28 20:10:22 +0100

    wiki section styles

--- src/Wiki/components/WikiContentSection/wiki_section.jsx
+++ src/Wiki/components/WikiContentSection/wiki_section.jsx
@@ -4,29 +4,32 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import MouseOverPopoverHtml from '../PopoverHtml/popover';
 import MouseOverPopoverCss from '../PopoverCss/popover';
 
-
 const WikiContentSection = (language) => (props) => {
   return (
     <div className="wiki__sections">
-      <div className="wiki__section">
-        
-        <h2 className="wiki__section-header">{props.header}</h2>
-        <h3 className="wiki_section_subHeader">{props.subHeader}</h3>
-        <p className="wiki__section-text">{props.text}</p>
+      <div className="wiki__section1">
+        <h2 className="wiki__section1-header">{props.header}</h2>
+        <h3 className="wiki_section1_subHeader">{props.subHeader}</h3>
+        <p className="wiki__section1-text">{props.text}</p>
       </div>
-      
-      <div className="wiki__section">
-      <MouseOverPopoverHtml />
-      <div className="wiki__section-border">
-      <SyntaxHighlighter language={language} >{props.snippetText}</SyntaxHighlighter>
-      </div>
-      <MouseOverPopoverCss />
-      </div>
-      <div className="wiki__section" dangerouslySetInnerHTML={{__html: props.snippetText}}>
+      <div className="wiki__section2">
+        <div className="wiki__section2-item">
+          <MouseOverPopoverHtml />
+          <div className="wiki__section2-border">
+            <SyntaxHighlighter language={language}>
+              {props.snippetText}
+            </SyntaxHighlighter>
+          </div>
+          <MouseOverPopoverCss />
+        </div>
+        <div
+          className="wiki__section2-item"
+          dangerouslySetInnerHTML={{ __html: props.snippetText }}
+        ></div>
       </div>
     </div>
   );
 };
 
-export const HtmlWikiContentSection = WikiContentSection("html")
-export const CssWikiContentSection = WikiContentSection("css")
+export const HtmlWikiContentSection = WikiContentSection('html');
+export const CssWikiContentSection = WikiContentSection('css');
