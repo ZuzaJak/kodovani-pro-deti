8c67bcaa2b9ebb9590189c0c20cc9abc9d4ed674
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-11-29 11:30:56 +0100

    certificate

--- src/Game/Certificate/certificate.jsx
+++ src/Game/Certificate/certificate.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import certificate from "../../img/diplom.svg"
 ​
 function resizeCanvas(canvas) {
   const { width, height } = canvas.getBoundingClientRect();
@@ -18,10 +19,9 @@ function resizeCanvas(canvas) {
 function draw(context, canvas) {
   resizeCanvas(canvas);
 ​
-  context.fillStyle = "#ff0000";
-  context.beginPath();
-  context.arc(50, 100, 20, 0, 2 * Math.PI);
+ /*  context.beginPath();
   context.fill();
+  context. */
   context.fillText('Text', canvas.width / 2, canvas.height / 2)
 }
 ​
@@ -35,9 +35,11 @@ const Certificate = (props) => {
   };
 ​
   return (
+    
     <div
       style={{ display: "flex", flexDirection: "column", minHeight: "50vh" }}
     >
+      <img id="diplom" width="220" height="277" src={certificate} alt="diplom" />
       <canvas ref={canvasRef} style={{ flex: "1" }}></canvas>
       <button onClick={render}>Render</button>
     </div>
