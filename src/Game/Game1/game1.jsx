9e3fff73484d040ef19f9b489074ecd78152642b
Zuzana Jakubčáková <zuzkajakubcak@gmail.com>
2020-12-03 06:19:51 +0100

    reload v game stále nefunguje

--- src/Game/Game1/game1.jsx
+++ src/Game/Game1/game1.jsx
@@ -47,9 +47,14 @@ const Game1 = () => {
             Hurá! Povedlo se ti to 😊 Klikni sem pro další hru!
           </Link>
         ) : (
-          <button className="reload" onClick={reload}>
+          <Link
+            className="reload"
+            to="/vyzkousej-to/1"
+            onClick={reload}
+            style={{ textDecoration: 'none' }}
+          >
             Modrou jsi netrefil/a, klikni 👉 sem pro další pokus!
-          </button>
+          </Link>
         )}
       </h2>
       <div className="game1__container">
